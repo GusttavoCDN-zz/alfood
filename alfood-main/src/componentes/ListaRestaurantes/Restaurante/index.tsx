671ab5910e71ab69eeb0d64a121f257e2c4c40ea
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-06-13 18:41:03 -0300

    refactor: Componente de restaurante renderizando pratos de acordo com busca na API

--- alfood-main/src/componentes/ListaRestaurantes/Restaurante/index.tsx
+++ alfood-main/src/componentes/ListaRestaurantes/Restaurante/index.tsx
@@ -1,21 +1,40 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import IPrato from '../../../interfaces/IPrato';
 import IRestaurante from '../../../interfaces/IRestaurante';
 import Prato from '../Prato';
 import estilos from './Restaurante.module.scss';
 
 interface RestauranteProps {
-  restaurante: IRestaurante
+  restaurante: IRestaurante;
 }
 
 const Restaurante = ({ restaurante }: RestauranteProps) => {
+  const [pratos, setPratos] = useState<IPrato[]>([]);
+  const { id } = restaurante;
 
-  return (<section className={estilos.Restaurante}>
-    <div className={estilos.Titulo}>
-      <h2>{restaurante.nome}</h2>
-    </div>
-    <div>
-      {restaurante.pratos?.map(item => <Prato prato={item} key={item.id} />)}
-    </div>
-  </section>)
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      const URL = `http://localhost:8000/api/v1/restaurantes/${id}/pratos/`;
+      const response = await axios.get(URL);
+      const data = response.data;
+      setPratos(data);
+    };
+    fetchData();
+  }, [id]);
 
-export default Restaurante
+  return (
+    <section className={estilos.Restaurante}>
+      <div className={estilos.Titulo}>
+        <h2>{restaurante.nome}</h2>
+      </div>
+      <div>
+        {pratos?.map((prato) => (
+          <Prato prato={prato} key={prato.id} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Restaurante;
