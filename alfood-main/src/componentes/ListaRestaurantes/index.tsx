cce42898c1aaef0b9b397458d54fc7377dafc265
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-06-14 01:58:16 -0300

    refactor: Refatorado busca de listas de restaurantes

--- alfood-main/src/componentes/ListaRestaurantes/index.tsx
+++ alfood-main/src/componentes/ListaRestaurantes/index.tsx
@@ -8,20 +8,34 @@ import { IPagination } from '../../interfaces/IPagination';
 const ListaRestaurantes = () => {
   const [restaurants, setRestaurants] = useState<IRestaurante[]>([]);
   const [nextPage, setNextPage] = useState('');
+  const [previousPage, setPreviousPage] = useState('');
 
-  async function fetchData() {
-    const URL = nextPage || 'http://localhost:8000/api/v1/restaurantes/';
+  const fetchData = async (page = '') => {
+    const URL = page || 'http://localhost:8000/api/v1/restaurantes/';
     const response = await axios.get<IPagination<IRestaurante>>(URL);
     const data = response.data.results;
-    setRestaurants([...restaurants, ...data]);
+    setRestaurants([...data]);
     setNextPage(response.data.next);
+    setPreviousPage(response.data.previous);
   }
-
+  
   useEffect(() => {
+    // const fetchData = async () => {
+    //   const URL = 'http://localhost:8000/api/v1/restaurantes/';
+    //   const response = await axios.get<IPagination<IRestaurante>>(URL);
+    //   const data = response.data.results;
+    //   setRestaurants([...data]);
+    //   setNextPage(response.data.next);
+    //   setPreviousPage(response.data.previous);
+    // }
+
     fetchData();
   }, []);
 
-  const showMoreRestaurants = async () => fetchData();
+  const showMoreRestaurants = async () => {
+    if (nextPage) fetchData(nextPage);
+    else fetchData(previousPage);
+  }
 
   return (
     <section className={style.ListaRestaurantes}>
@@ -32,6 +46,7 @@ const ListaRestaurantes = () => {
         <Restaurante restaurante={item} key={item.id} />
       ))}
       {nextPage && <button onClick={showMoreRestaurants}>ver mais</button>}
+      {previousPage && <button onClick={showMoreRestaurants}>ver menos</button>}
     </section>
   );
 };
