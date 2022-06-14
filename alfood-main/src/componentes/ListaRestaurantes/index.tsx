a87dab2d5a32a3ec9ba952e5f2e9abd6258cb5bd
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-06-14 02:52:22 -0300

    feat: Adicionado filtro de busca de restaurantes

--- alfood-main/src/componentes/ListaRestaurantes/index.tsx
+++ alfood-main/src/componentes/ListaRestaurantes/index.tsx
@@ -4,9 +4,14 @@ import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 import { IPagination } from '../../interfaces/IPagination';
+import { TextField } from '@mui/material';
 
 const ListaRestaurantes = () => {
   const [restaurants, setRestaurants] = useState<IRestaurante[]>([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState<
+    IRestaurante[]
+  >([]);
+  const [restaurant, setRestaurant] = useState('');
   const [nextPage, setNextPage] = useState('');
   const [previousPage, setPreviousPage] = useState('');
 
@@ -17,36 +22,46 @@ const ListaRestaurantes = () => {
     setRestaurants([...data]);
     setNextPage(response.data.next);
     setPreviousPage(response.data.previous);
-  }
-  
-  useEffect(() => {
-    // const fetchData = async () => {
-    //   const URL = 'http://localhost:8000/api/v1/restaurantes/';
-    //   const response = await axios.get<IPagination<IRestaurante>>(URL);
-    //   const data = response.data.results;
-    //   setRestaurants([...data]);
-    //   setNextPage(response.data.next);
-    //   setPreviousPage(response.data.previous);
-    // }
+  };
 
+  useEffect(() => {
     fetchData();
   }, []);
+
+  useEffect(() => {
+    const restaurantesFilteredByName = restaurants.filter(({ nome }) => {
+      const name = nome.toLowerCase();
+      return name.includes(restaurant);
+    });
+
+    console.log(restaurantesFilteredByName);
+
+    setFilteredRestaurants(restaurantesFilteredByName);
+  }, [restaurant, restaurants]);
 
   const showMoreRestaurants = async () => {
     if (nextPage) fetchData(nextPage);
     else fetchData(previousPage);
-  }
+    setRestaurant('');
+  };
 
   return (
     <section className={style.ListaRestaurantes}>
       <h1>
         Os restaurantes mais <em>bacanas</em>!
       </h1>
-      {restaurants?.map((item) => (
+      <TextField
+        value={restaurant}
+        label="Pesquise um Restaurante"
+        variant="standard"
+        onChange={(event) => setRestaurant(event.target.value)}
+      />
+      {filteredRestaurants?.map((item) => (
         <Restaurante restaurante={item} key={item.id} />
       ))}
-      {nextPage && <button onClick={showMoreRestaurants}>ver mais</button>}
-      {previousPage && <button onClick={showMoreRestaurants}>ver menos</button>}
+      <button onClick={showMoreRestaurants}>
+        {nextPage ? 'ver mais' : 'voltar'}
+      </button>
     </section>
   );
 };
