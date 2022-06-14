e3af846603091e19a6d090efe9805fdf52eb48b4
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-06-14 14:25:12 -0300

    feat: Criado filtro de texto

--- alfood-main/src/componentes/ListaRestaurantes/index.tsx
+++ alfood-main/src/componentes/ListaRestaurantes/index.tsx
@@ -1,10 +1,16 @@
-import { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 import { IPagination } from '../../interfaces/IPagination';
-import { TextField } from '@mui/material';
+import {
+  Button,
+  FormControl,
+  MenuItem,
+  Select,
+  TextField,
+} from '@mui/material';
 
 const ListaRestaurantes = () => {
   const [restaurants, setRestaurants] = useState<IRestaurante[]>([]);
@@ -14,27 +20,52 @@ const ListaRestaurantes = () => {
   const [restaurant, setRestaurant] = useState('');
   const [nextPage, setNextPage] = useState('');
   const [previousPage, setPreviousPage] = useState('');
+  const [order, setOrder] = useState('nome');
 
   const fetchData = async (page = '') => {
     const URL = page || 'http://localhost:8000/api/v1/restaurantes/';
     const response = await axios.get<IPagination<IRestaurante>>(URL);
     const data = response.data.results;
     setRestaurants([...data]);
+    setFilteredRestaurants([...data]);
     setNextPage(response.data.next);
     setPreviousPage(response.data.previous);
   };
+
+  const orderRestaurants = useCallback(
+    (restaurants: IRestaurante[]) => {
+      if (order === 'nome')
+        return restaurants.sort((a, b) => a[order].localeCompare(b[order]));
+      if (order === 'id')
+        return restaurants.sort((a, b) => a[order] - b[order]);
+      return restaurants;
+    },
+    [order]
+  );
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  useEffect(() => {
+  const searchRestaurants = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
     const restaurantesFilteredByName = restaurants.filter(({ nome }) => {
       const name = nome.toLowerCase();
-      return name.includes(restaurant);
+      return name.includes(restaurant.toLowerCase());
     });
-    setFilteredRestaurants(restaurantesFilteredByName);
-  }, [restaurant, restaurants]);
+
+    const restaurantsOrdered = orderRestaurants(restaurantesFilteredByName);
+    setFilteredRestaurants(restaurantsOrdered);
+  };
+
+  // useEffect(() => {
+  //   const restaurantesFilteredByName = restaurants.filter(({ nome }) => {
+  //     const name = nome.toLowerCase();
+  //     return name.includes(restaurant);
+  //   });
+  //   setFilteredRestaurants(restaurantesFilteredByName);
+  // }, [restaurant, restaurants]);
 
   const showMoreRestaurants = async () => {
     if (nextPage) fetchData(nextPage);
@@ -47,18 +78,36 @@ const ListaRestaurantes = () => {
       <h1>
         Os restaurantes mais <em>bacanas</em>!
       </h1>
-      <TextField
-        value={restaurant}
-        label="Pesquise um Restaurante"
-        variant="standard"
-        onChange={(event) => setRestaurant(event.target.value)}
-      />
+      <FormControl
+        sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}
+        size="small"
+        component="form">
+        <TextField
+          value={restaurant}
+          label="Pesquise um Restaurante"
+          variant="standard"
+          onChange={(event) => setRestaurant(event.target.value)}
+          sx={{ marginRight: '1rem' }}
+        />
+        <Select
+          value={order}
+          onChange={({ target }) => setOrder(target.value)}
+          label="Ordem"
+          variant="standard"
+          sx={{ marginRight: '1rem' }}>
+          <MenuItem value="nome">nome</MenuItem>
+          <MenuItem value="id">id</MenuItem>
+        </Select>
+        <Button variant="outlined" onClick={searchRestaurants}>
+          Buscar
+        </Button>
+      </FormControl>
       {filteredRestaurants?.map((item) => (
         <Restaurante restaurante={item} key={item.id} />
       ))}
-      <button onClick={showMoreRestaurants}>
+      <Button onClick={showMoreRestaurants}>
         {nextPage ? 'ver mais' : 'voltar'}
-      </button>
+      </Button>
     </section>
   );
 };
